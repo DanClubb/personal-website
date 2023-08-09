9d46f05aeb313b3e6f689b7b5b1b8eed522f329b
daniel-clubb <danclubb004@gmail.com>
2023-08-09 15:31:42 +0100

    add og:image

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import Header from "./components/Header";
 import { NavContextProvider } from "./context/nav-context";
 import Footer from "./components/Footer";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +16,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta property="og:image" content="../public/hero-devices.svg" />
+      </head>
       <body className={inter.className}>
         <NavContextProvider>
           <Header />
