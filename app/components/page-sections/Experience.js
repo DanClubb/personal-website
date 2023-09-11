f717ee40040907029ce8ccc3b0a2b03a0dcec871
daniel-clubb <danclubb004@gmail.com>
2023-09-11 13:14:15 +0100

    updated experience section

--- app/components/page-sections/Experience.js
+++ app/components/page-sections/Experience.js
@@ -3,24 +3,33 @@ function Experience() {
     <div className="experience__card">
       <h3 className="experience__title">Zone | September 2021 - July 2023</h3>
       <p className="experience__description">
-        Zone is a digital agency specializing in the design, development, and
-        implementation of digital solutions. Zone focuses on helping clients
-        across various industries create and optimize their digital presence to
-        engage and connect with their target audience.
+        Zone focuses on helping clients across various industries create and
+        optimize their digital presence to engage and connect with their target
+        audience.
       </p>
 
       <p className="experience__description">
-        During my time at Zone I worked on a couple of client projects.{" "}
-        <strong>Elextrolux</strong> on their online retail store, and{" "}
-        <strong>Greeneking</strong> on their online booking journey allowing
-        users to book a table or occasion
+        Zone has given me almost 2 years of professional experience as a
+        frontend developer. During this time I worked large scale client
+        projects. <strong>Elextrolux</strong> on their online retail store, and{" "}
+        <strong>Greene King</strong> on their online booking journey allowing
+        users to book a table or occasion. I also spent time working on an
+        <strong> internal car park booking system</strong> for Zone employees.
+      </p>
+      <p>
+        I grew a lot in this time both as a developer and as a person which is
+        refelected in positive feedback I received which often highlighted that
+        I learn quickly and have potential to continue to grow as a developer as
+        well as my ability to build strong relationships and work as part of a
+        team.
       </p>
       <div className="responsibilities">
         <h3>My responsibilities included:</h3>
         <ul className="responsibilities-list">
           <li>
-            Developing and maintaining web applications using React.js and other
-            related technologies
+            Developing and maintaining web applications using tehcnologies such
+            as{" "}
+            <strong>React.js, Express.js, SCSS, TailwindCSS, Cypress.</strong>
           </li>
           <li>
             Collaborating with cross-functional teams including designers,
