68df4e739490e8da31b2304be57bdcbeed4c91eb
daniel-clubb <danclubb004@gmail.com>
2023-12-02 17:20:20 +0000

    sorted out footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,19 +1,49 @@
+import IMAGES from "@/public/images/Images";
+import Image from "next/image";
+
 function Footer() {
-  return (
-    <footer>
-      <div>
-        <h4>I am Daniel Clubb</h4>
-        <p>I am a software developer</p>
-      </div>
-      <div>
-        <ul>
-          <li>GitHub (icon)</li>
-          <li>LinkedIn (icon)</li>
-        </ul>
-      </div>
-      <div>Next.js v13.4.0 | CSS3</div>
-    </footer>
-  );
+    return (
+        <footer>
+            <div className="footer_personal-info">
+                <div>
+                    <h4>I'm Daniel Clubb</h4>
+                    <p>software developer</p>
+                </div>
+                <div>
+                    <ul className="footer_links">
+                        <li>
+                            <a
+                                href="https://github.com/DanClubb"
+                                target="_blank"
+                            >
+                                <Image
+                                    src={IMAGES.githubLogo}
+                                    width={30}
+                                    height={30}
+                                />
+                                GitHub
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="https://linkedin.com/in/dan-clubb"
+                                target="_blank"
+                            >
+                                <Image
+                                    src={IMAGES.LinkedIn}
+                                    width={30}
+                                    height={30}
+                                />
+                                LinkedIn
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className="footer_site-tech">Next.js v13.4.0 | CSS3</div>
+        </footer>
+    );
 }
 
 export default Footer;
--- public/images/Images.js
+++ public/images/Images.js
@@ -2,6 +2,7 @@ const IMAGES = {
     placeholder: require("./placeholder.svg").default,
     cv: require("./cv-image.png").default,
     githubLogo: require("./github-mark.png").default,
+    LinkedIn: require("./LinkedIn.png").default,
     heroDevices: require("./hero-devices.png").default,
     TicTacToe: require("./tictactoe.png").default,
     FrontendMentorChallenges: require("./frontend-mentor.png").default,
