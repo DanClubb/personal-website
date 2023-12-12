d9c3e8c15f80239f5a2a7254a647bddf67fa2f6d
daniel-clubb <danclubb004@gmail.com>
2023-12-12 16:28:05 +0000

    convert some png to webp, removed unwanted images

--- public/images/Images.js
+++ public/images/Images.js
@@ -1,12 +1,11 @@
 const IMAGES = {
     placeholder: require("./placeholder.svg").default,
-    cv: require("./cv-image.png").default,
     githubLogo: require("./github-mark.png").default,
     LinkedIn: require("./LinkedIn.png").default,
-    heroDevices: require("./hero-devices.png").default,
-    TicTacToe: require("./tictactoe.png").default,
-    FrontendMentorChallenges: require("./frontend-mentor.png").default,
-    noteshare: require("./noteshare.png").default,
+    heroDevices: require("./hero-devices.webp").default,
+    TicTacToe: require("./tictactoe.webp").default,
+    FrontendMentorChallenges: require("./frontend-mentor.webp").default,
+    noteshare: require("./noteshare.webp").default,
     arrow: require("./icon-arrow-down.svg").default,
 };
 
