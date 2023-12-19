1cc94132c8a9533db34d40b9934a34d6c4f85643
daniel-clubb <danclubb004@gmail.com>
2023-12-19 20:44:08 +0000

    added full name to hero and changed hero heading font size on mobile

--- app/components/page-sections/Hero.js
+++ app/components/page-sections/Hero.js
@@ -3,34 +3,36 @@ import { Fragment } from "react";
 import heroDevices from "../../../public/hero-devices.svg";
 
 function Hero() {
-  return (
-    <Fragment>
-      <div className="hero__text">
-        <h1 className="hero__text--header">
-          Hi, I&apos;m <span className="hero__text--name">Daniel</span>
-        </h1>
-        <h2 className="hero__text--sub-header">Frontend Developer</h2>
-        <p className="hero__text--paragraphy">
-          I am a frontend developer who loves problem solving while creating
-          complex functionality and implementing designs with pixel-perfect
-          accuracy.
-          <br />
-          Away from the keyboard I enjoy learning new skills, keeping fit and
-          recharging my batteries while socialising with friends
-        </p>
-      </div>
+    return (
+        <Fragment>
+            <div className="hero__text">
+                <h1 className="hero__text--header">
+                    Hi, I&apos;m{" "}
+                    <span className="hero__text--name">Daniel Clubb</span>
+                </h1>
+                <h2 className="hero__text--sub-header">Frontend Developer</h2>
+                <p className="hero__text--paragraphy">
+                    I am a frontend developer who loves problem solving while
+                    creating complex functionality and implementing designs with
+                    pixel-perfect accuracy.
+                    <br />
+                    Away from the keyboard I enjoy learning new skills, keeping
+                    fit and recharging my batteries while socialising with
+                    friends
+                </p>
+            </div>
 
-      <div className="hero__devices-container">
-        <Image
-          src={heroDevices}
-          fill={true}
-          alt="collection of devices I use while working"
-          className="hero__devices"
-          priority
-        />
-      </div>
-    </Fragment>
-  );
+            <div className="hero__devices-container">
+                <Image
+                    src={heroDevices}
+                    fill={true}
+                    alt="collection of devices I use while working"
+                    className="hero__devices"
+                    priority
+                />
+            </div>
+        </Fragment>
+    );
 }
 
 export default Hero;
